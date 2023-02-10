58d72225ddf7767474f2a7fdc869bfa1d816ec16
Christian Jeff L. Gabion <119904646+Clearingguy@users.noreply.github.com>
2023-02-10 11:48:59 +0800

    Create Tiger.java

---
+++ Exercise6/Tiger.java
@@ -0,0 +1,9 @@
+public class Tiger implements Animal{
+    public void walk(){
+        System.out.println("The Tiger is walking");
+    }
+    public void run(){
+        System.out.println("The Tiger is running");
+    }
+       
+}
