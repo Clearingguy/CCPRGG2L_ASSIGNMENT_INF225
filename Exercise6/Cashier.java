0030ed4f433bea5651a3993b59b4e23e0336db2d
Christian Jeff L. Gabion <119904646+Clearingguy@users.noreply.github.com>
2023-02-10 11:46:39 +0800

    Create Cashier.java

---
+++ Exercise6/Cashier.java
@@ -0,0 +1,11 @@
+public class Cashier {
+
+    public void checkOut(GroceryItem item) {
+        System.out.println(item.name+" has been bought.");
+
+    }
+
+
+public void showItemPrice(GroceryItem item){
+System.out.println(item.price);
+}}
